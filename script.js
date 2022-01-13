ea49710e4ad786f963fb90fb2c6122e3dbd27409
hasan <67376611+BantuMasyarakat@users.noreply.github.com>
2022-01-13 12:35:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -91,9 +91,7 @@ document.querySelector("section.claim form button").onclick = () => {
         document.querySelector("section.claim form .input-g input[name='email']").value !== ""
     ) {
         const xhr = new XMLHttpRequest();
-        
         xhr.open("get", "https://server-app.herokuapp.com/get");
-        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
         xhr.send();
 
         xhr.onreadystatechange = () => {
